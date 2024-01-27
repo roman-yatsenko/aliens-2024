b24a115cba0c2e1676bde7a2907a6f73598baff3
YaRo <roman.yatsenko@hneu.net>
2024-01-27 14:17:42 +0200

    Refactor _create_fleet

--- start.py
+++ start.py
@@ -66,6 +66,14 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _create_alien(self, alien_number):
+        # Створення прибульця і розміщення його в ряду
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
+
     def _create_fleet(self):
         """Створення флоту прибульців"""
         # Створення прибульця і визначення кількості прибульців в ряду
@@ -76,11 +84,7 @@ class AlienInvasion:
 
         # Створення першого ряду прибульців
         for alien_number in range(number_aliens_x):
-            # Створення прибульці і розміщення його в ряду
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
 
     def _fire_bullet(self):
         """Створює новий снаряд та додає його до групи bullets"""
