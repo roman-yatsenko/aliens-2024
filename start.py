c1e7271a79004598a28e63bae576c78a91b6a2cf
YaRo <roman.yatsenko@hneu.net>
2024-02-03 14:45:02 +0200

    Add game over

--- start.py
+++ start.py
@@ -37,9 +37,11 @@ class AlienInvasion:
         while True:
             # Відслідковування подій клавіфатури та миші
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
 
             # За кожної ітерації циклу перемальовується екран
             self._update_screen()
@@ -131,19 +133,22 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Обробляє зіткнення корабля з прибульцем"""
-        # Зменшення ships_left
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Зменшення ships_left
+            self.stats.ships_left -= 1
 
-        # Очищення списків прибульців та снарядів
-        self.aliens.empty()
-        self.bullets.empty()
+            # Очищення списків прибульців та снарядів
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Створення нового флоту та центрування корабля
-        self._create_fleet()
-        self.ship.center_ship()
+            # Створення нового флоту та центрування корабля
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Пауза
-        sleep(0.5)
+            # Пауза
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _update_aliens(self):
         """Оновлює позиції всіх прибульців флоту"""
