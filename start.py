62b452ecad381e352f0849f6e20e700669a9911c
YaRo <roman.yatsenko@hneu.net>
2024-02-10 14:51:39 +0200

    Add speed restart

--- start.py
+++ start.py
@@ -118,6 +118,7 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
             # Скидання ігрової статистики
+            self.settings.initialyze_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
